9ef245c521ad7b8bb190a967d62c42414dd76a57
Allan Mendoza <allanmaleman@gmail.com>
2022-03-24 20:15:37 -0600

    Se tiene lista la propuesta de la grilla para las imagenes de Tesoros y tambien la estructura de la interfza de la pagina de lets grow together

--- vista/modulosENG/servicio1.php
+++ vista/modulosENG/servicio1.php
@@ -1,30 +1,135 @@
 <!-- welcome_docmed_area_start -->
 <div class="welcome_docmed_area" id="internship">
     <div class="container">
-        <div class="row">
-            <div class="col-xl-6 col-lg-6">
-                <div class="welcome_thumb">
-                    <!-- <div class="thumb_1">
-                        <img src="vista/img/about/1.png" alt="">
-                    </div>
-                    <div class="thumb_2">
-                        <img src="vista/img/about/2.png" alt="">
-                    </div> -->
-                    <img src="vista/img/about/v1.png" style="width:100%" alt="">
-                </div>
+        <div class="row align-items-start">
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
             </div>
-            <div class="col-xl-6 col-lg-6">
-                <div class="welcome_docmed_info">
-                    <h2 style="font-size:2em;">Internship Description</h2>
-                    <p>Tesoros de Dios offers internship opportunities for college-age students to come and learn from Tesoros de Dios while making real-life connections to their studies. Interns help the operations of our Managua office and, ultimately, help Tesoros de Dios continue its vision of helping every child with disabilities reach their full potential. During this time, interns will grow spiritually and develop as leaders while having fun! Interns will be immersed in the Spanish language and Nicaraguan culture, and they will have opportunities to explore other parts of the country.</p>
-                    <ul>
-                        <li> <i class="flaticon-right"></i> Interns MUST be at least 18 years old. </li>
-                        <li> <i class="flaticon-right"></i> Time commitment is 12 weeks</li>
-                    </ul>
-                    <p>If you are interested in becoming an intern at Tesoros de Dios, please fill out the form.</p>
-                    <a href="#formulary" class="boxed-btn3-white-2">Form</a>
-                </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
             </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="vista/img/testmonial/foto.jpg" alt="img" class="w-100 img-responsive">
+                <!-- <div class="btn-group"> -->
+                <button type="submit" class="btn btn-success w-100">See More</button>
+                <button type="button" class="btn btn-info w-100" data-dismiss="modal">Make a Sponsor</button>
+                <!-- </div> -->
+            </div>
+
         </div>
     </div>
 </div>
