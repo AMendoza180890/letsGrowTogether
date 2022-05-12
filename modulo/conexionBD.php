d8c0227c5c5885c1172d640e4a0fe4f2b6a8d7ca
Allan Mendoza <allanmaleman@gmail.com>
2022-05-11 20:31:15 -0600

    Se actualizan cambios del proyecto actual a la version publicada

--- modulo/conexionBD.php
+++ modulo/conexionBD.php
@@ -2,7 +2,7 @@
 class conexionBD{
     public static function conexion(){
         try {
-            $bd = new PDO("mysql:host=localhost;dbname=bdtogether","root","");
+            $bd = new PDO("mysql:host=localhost;dbname=tesorosd_letsgrowtogether", "tesorosd_lgtgthe", "MWr^PW75~{2r");
             $bd -> exec("set names utf8");
 
             return $bd;
